26a9e1078bb133d665584be72426da85e20a1752
Matthew Pickering <matthewtpickering@gmail.com>
2019-07-05 11:22:44 +0100

    init

---
+++ default.nix
@@ -0,0 +1,18 @@
+let
+  pin = import ((import ./nix/sources.nix).nixpkgs) {} ;
+  # Import the Haskell.nix library,
+  haskell = import ((import ./nix/sources.nix)."haskell.nix") { pkgs = pin; };
+
+  pkgPlan = haskell.callCabalProjectToNix
+              { index-state = "2019-05-10T00:00:00Z"
+              ; src = pin.lib.cleanSource ./.;};
+
+  # Instantiate a package set using the generated file.
+  pkgSet = haskell.mkCabalProjectPkgSet {
+    plan-pkgs = import pkgPlan;
+    pkg-def-extras = [];
+    modules = [];
+  };
+
+in
+  { dyepack = pkgSet.config.hsPkgs.dyepack ; }

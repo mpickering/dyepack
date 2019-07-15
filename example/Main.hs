f78c9418d0c0bd602be2da45fc269d13512b3ce9
Luke Lau <luke_lau@icloud.com>
2019-07-15 20:27:27 +0100

    Tidy up

--- example/Main.hs
+++ example/Main.hs
@@ -5,21 +5,16 @@ module Main where
 import Debug.Dyepack
 import GHC.Generics
 
-data Foo = Foo Int String Bar
-  deriving Generic
-
-data Bar = Bar Float
-
 data A = A String deriving Show
 data User = User A Int deriving Generic
 
 main = do
   v <- getLine
-  let x = A v
-  let y = id [x,x,x]
-      u = User x 5
+  let a = A v
+      y = id [a, a, a]
+      u = User a 5
 
-  dyed <- mkDyed u
+  dyed <- dye u
   checkDyed dyed
   print y
 
--- src/Debug/Dyepack.hs
+++ src/Debug/Dyepack.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE RankNTypes, ExistentialQuantification #-}
 {-# LANGUAGE TypeApplications, FlexibleContexts #-}
 {-# LANGUAGE BangPatterns, MagicHash #-}
-module Debug.Dyepack (mkDyed, checkDyed, Dyed) where
+module Debug.Dyepack (dye, checkDyed, Dyed) where
 
 import qualified GHC.Generics as GHC
 import Generics.SOP
@@ -14,16 +14,23 @@ foreign import ccall unsafe "findPtr" findPtr :: Ptr a -> Int -> IO ()
 
 -- TODO: come up with a better name for Part
 data Part = forall a. Part (Weak a)
+
+-- | Represents an object who's contents on the heap have been "dyed".
+-- The dyed contents have weak pointers, which can then be used to check if they
+-- are being retained.
 newtype Dyed a = Dyed [Part]
 
-mkDyed :: (GHC.Generic a, GFrom a, All (All Top) (GCode a)) => a -> IO (Dyed a)
-mkDyed !x = do
+-- | Create a new 'Dyed' that can be then used with 'checkDyed'
+dye :: (GHC.Generic a, GFrom a, All (All Top) (GCode a)) => a -> IO (Dyed a)
+dye !x = do
   let parts :: [IO Part]
-      parts = hcfoldMap (Proxy @Top) go (gfrom x)
+      parts = hcollapse $ hcmap (Proxy @Top) (mapIK go) (gfrom x)
   Dyed <$> sequence parts
-  where go :: I a1 -> [IO Part]
-        go (I !y) = [Part <$> mkWeakPtr y Nothing]
+  where go :: b -> IO Part
+        go !y = Part <$> mkWeakPtr y Nothing
 
+-- | Check if a 'Dyed' is being retained. 
+-- Will print to `stderr` if it is being retained.
 checkDyed :: Dyed a -> IO ()
 checkDyed (Dyed parts) = do
   performGC
